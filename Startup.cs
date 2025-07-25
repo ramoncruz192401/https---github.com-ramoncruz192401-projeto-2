ff4c6e75b984dcd510c9bad473bc9ce83330cc1f
Ramon Alberto <ramoneletro@gmail.com>
2025-07-24 21:26:14 -0300

    site

--- Startup.cs
+++ Startup.cs
@@ -26,7 +26,7 @@ namespace MEUSITE
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Bem-vindo ao MeuSite!");
+                    await context.Response.WriteAsync("Bem-vindo ao Ramon!");
                 });
             });
         }
